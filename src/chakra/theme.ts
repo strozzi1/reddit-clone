6e52d29f8deb602fdc696795283cb29d86b41456
Joshua Strozzi <strozzij@oregonstate.edu>
2023-02-15 13:08:50 -0800

    Initial commit of Learning Project

---
+++ src/chakra/theme.ts
@@ -0,0 +1,28 @@
+// 1. import `extendTheme`
+import { extendTheme } from "@chakra-ui/react";
+import '@fontsource/open-sans/300.css';
+import '@fontsource/open-sans/400.css';
+import '@fontsource/open-sans/700.css';
+
+// 2. call `extendTheme` and pass your custom values
+export const theme = extendTheme({
+    colors: {
+        gray: {
+            100: '#FF3C00',
+        },
+    },
+    fonts: {
+        heading: "Roboto",
+        body: "Open Sans, sans-serif",
+    },
+    styles: {
+        global: () => ({
+            body: {
+                bg: 'gray.200',
+            },
+        }),
+    },
+    components: {
+        // Button
+    }
+})
